6fcc8de00aa9bb9d53b04ab58ea4e7648f8748b6
alexiscenriquez <alexiscenriquez@hotmail.com>
2023-02-28 15:03:38 -0600

    User Show

--- src/com/cognixia/jump/progresstracker/dao/UserShow.java
+++ src/com/cognixia/jump/progresstracker/dao/UserShow.java
@@ -1,5 +1,43 @@
 package com.cognixia.jump.progresstracker.dao;
 
 public class UserShow {
+private int userID;
+private int showID;
+private int rating;
+private int currEp;
+
+
+public UserShow(int userID, int showID, int rating, int currEp) {
+	super();
+	this.userID = userID;
+	this.showID = showID;
+	this.rating = rating;
+	this.currEp = currEp;
+}
+public int getUserID() {
+	return userID;
+}
+public void setUserID(int userID) {
+	this.userID = userID;
+}
+public int getShowID() {
+	return showID;
+}
+public void setShowID(int showID) {
+	this.showID = showID;
+}
+public int getRating() {
+	return rating;
+}
+public void setRating(int rating) {
+	this.rating = rating;
+}
+public int getCurrEp() {
+	return currEp;
+}
+public void setCurrEp(int currEp) {
+	this.currEp = currEp;
+}
+
 
 }
