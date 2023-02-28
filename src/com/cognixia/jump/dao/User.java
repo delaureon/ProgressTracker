f2b380d435f5c9c54d9f1c155e72bd3b9e76a1eb
Albert <albertzeap@gmail.com>
2023-02-28 11:18:26 -0800

    Created User POJO

---
+++ src/com/cognixia/jump/dao/User.java
@@ -0,0 +1,60 @@
+package com.cognixia.jump.dao;
+
+public class User {
+
+	
+	// User attributes
+	private int userId;
+	private String username;
+	private String password;
+	private int roleType;
+	
+	public User(int userId, String username, String password, int roleType) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.roleType = roleType;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(int roleType) {
+		this.roleType = roleType;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", roleType=" + roleType
+				+ "]";
+	}
+	
+	
+	
+}
