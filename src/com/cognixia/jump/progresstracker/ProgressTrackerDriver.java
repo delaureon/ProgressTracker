b7ee5fa2144c8b45611d251dcf0d82723ba764c5
Aren Lizardo <70218060+delaureon@users.noreply.github.com>
2023-03-01 08:55:03 -0800

    Updating user interface

--- src/com/cognixia/jump/progresstracker/ProgressTrackerDriver.java
+++ src/com/cognixia/jump/progresstracker/ProgressTrackerDriver.java
@@ -1,6 +1,7 @@
 package com.cognixia.jump.progresstracker;
 
 import java.lang.invoke.WrongMethodTypeException;
+
 import java.util.Scanner;
 
 import com.cognixia.jump.progresstracker.dao.*;
@@ -10,26 +11,33 @@ public class ProgressTrackerDriver {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		boolean flag = true;
-		while (flag == true) {
-			System.out.println("What is your username?");
+		int role = 0;
+		String username = "";
+		String password = "";
+		System.out.println("Welcome to the TV Show Tracker!");
+		while (!scan.equals("quit")) {
 			try {
-				// get username
-				// check username
-				// if user ain't there, redo
-			} catch (WrongMethodTypeException e) {
-				// sdfasdf
+				do {
+					System.out.println("Please enter 0 if you're a user or 1 if you're an admin:");
+					role = scan.nextInt();
+					System.out.println("What is your username?");
+					username = scan.nextLine();
+					System.out.println("What is your password?");
+					password = scan.nextLine();
+					checkUser(role, username, password);
+					if (checkUser(role, username, password) == false) {
+						System.out.println("The data you have entered does not match. Please try again.");
+					}
+				} while (checkUser(role, username, password) != true);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-		
-		
-
 	}
 	
-	public boolean checkUserRole(User user) {
-		if (user.getRoleType() == 1) {
-			return true;
-		}
+	public static boolean checkUser(int role, String username, String password) {
+		// check user data with database
+		// return true;
 		return false;
 	}
 
