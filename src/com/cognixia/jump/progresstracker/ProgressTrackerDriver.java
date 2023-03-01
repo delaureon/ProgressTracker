7ae61afcbdffabafeca99f8fba44524f4efb2a31
Aren Lizardo <70218060+delaureon@users.noreply.github.com>
2023-03-01 08:51:13 -0800

    Adding user authenticate menu

--- src/com/cognixia/jump/progresstracker/ProgressTrackerDriver.java
+++ src/com/cognixia/jump/progresstracker/ProgressTrackerDriver.java
@@ -1,10 +1,36 @@
 package com.cognixia.jump.progresstracker;
 
+import java.lang.invoke.WrongMethodTypeException;
+import java.util.Scanner;
+
+import com.cognixia.jump.progresstracker.dao.*;
+
 public class ProgressTrackerDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		boolean flag = true;
+		while (flag == true) {
+			System.out.println("What is your username?");
+			try {
+				// get username
+				// check username
+				// if user ain't there, redo
+			} catch (WrongMethodTypeException e) {
+				// sdfasdf
+			}
+		}
+		
+		
 
+	}
+	
+	public boolean checkUserRole(User user) {
+		if (user.getRoleType() == 1) {
+			return true;
+		}
+		return false;
 	}
 
 }
