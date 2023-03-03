2c210fb124805d90459f337ef5d1854d361d141f
Albert Paez <46942854+albertzeap@users.noreply.github.com>
2023-03-03 08:38:56 -0800

    update sqlscript

--- CreateScripts.sql
+++ CreateScripts.sql
@@ -1,4 +1,4 @@
-create database progresstracker;
+-- create database progresstracker;
 use progresstracker;
 create table Users(
 UserID int auto_increment primary key ,
@@ -6,26 +6,20 @@ UserName varchar(255),
 Password varchar(255),
 RoleType int
 );
-
 create table Shows(
 ShowID int auto_increment primary key ,
 Name varchar(255),
 Descript text,
 TotalEps int);
-
 create table progress(
 progressID int primary key auto_increment,
 progress varchar(255)
 );
-
 insert into progress values(null, "Not Started");
 insert into progress values(null, "In Progress");
 insert into progress values(null, "Complete");
-
-
-
 create table Users_Shows(
-UserID int, 
+UserID int,
 ShowID int,
 ProgressID int,
 Rating int,
@@ -47,38 +41,37 @@ insert into Shows(Name, Descript, TotalEps)
 	values("Grey's Anatomy", "Show about dramatic doctors.", 205);
 insert into Shows(Name, Descript, TotalEps)
 	values("Spongebob", "Show about a sponge.", 111);
-    
+   insert into Shows(Name, Descript, TotalEps)
+	values("Manifest","Show about a disappering plane",40);
+     insert into Shows(Name, Descript, TotalEps)
+	values("Wednesday","Show about Wednesday Addams",8);
+       insert into Shows(Name, Descript, TotalEps)
+	values("The Bear","Show about saving a local restaurant",10);
+       insert into Shows(Name, Descript, TotalEps)
+	values("You","Show about a stalker",60);
+       insert into Shows(Name, Descript, TotalEps)
+	values("1899","Show about a disappering ship",10);
+       insert into Shows(Name, Descript, TotalEps)
+	values("Vikings","Show about vikings",80);
+       insert into Shows(Name, Descript, TotalEps)
+	values("Money Heist","Show about a bank robbers",40);
 insert into Users_Shows(UserID, ProgressID,ShowID, Rating, CurrentEp)
 	values(1, 2,3, 5, 1);
 insert into Users_Shows(UserID, ProgressID,ShowID, Rating, CurrentEp)
 	values(1,2, 2, 5, 30);
-    
 -- Albert watches Walking Dead, Spongebob
 insert into Users_Shows(UserID, ProgressID, ShowID, Rating, CurrentEp)
 	values(2,2, 1, 2, 30);
 insert into Users_Shows(UserID,ProgressID, ShowID, Rating, CurrentEp)
 	values(2,2, 3, 2, 60);
-
 -- Alexis watches Grey's Anatomy, Walking Dead
 insert into Users_Shows(UserID, ProgressID, ShowID, Rating, CurrentEp)
 	values(3, 2,2, 3, 100);
 insert into Users_Shows(UserID, ProgressID,ShowID, Rating, CurrentEp)
 	values(3,2, 1, 3, 100);
-    
-select * from users_shows;
-
 select Users_Shows.ShowID, progress.progress,Users.Userid,Name, CurrentEp,Rating , totalEps, ((CurrentEp /totalEps)*100) as percentcomplete
 from users
 join Users_Shows on users.UserID=Users_Shows.UserID
 join progress on Users_Shows.ProgressID=progress.ProgressID
 join Shows on Users_Shows.ShowID=Shows.ShowID
-where Users.userid=2;
-
-Update users_shows set ProgressID=1,Rating=3,CurrentEp=1 Where UserID=2 and ShowID = 5;
-
-select * from users_shows where ShowID = 5 and UserID=2;
-
-select * from users;
-select * from shows;
-select * from users_shows;
-select * from progress;
+where Users.userid=1;
